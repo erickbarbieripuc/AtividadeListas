e5785c00e76ebbc0118133e8b67c8a46541ff9e6
erickbarbieripuc <167023009+erickbarbieripuc@users.noreply.github.com>
2024-04-23 21:40:18 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# #Atividade 1,2 e 3
+# a = []
+# for i in range (10):
+#     numero = input("Digite um numero inteiro:")
+#     a.append(numero)
+#
+# menorValor = min(a)
+# maiorValor = max(a)
+#
+# print(f"O maior e menor valor da lista são respectivamente: {maiorValor} e {menorValor}")
+
+# #Atividade 4
+# numeroAlunos = int(input("Quantidade de alunos:"))
+# mediaPositiva = []
+# mediaNegativa = []
+# i = 0
+# while i < numeroAlunos:
+#     nota = float(input("Digite a nota do aluno:"))
+#     i += 1
+#     if nota >= 60:
+#         mediaPositiva.append(nota)
+#     else:
+#         mediaNegativa.append(nota)
+#
+# aMediaPositiva = len(mediaPositiva)
+# aMediaNegativa = len(mediaNegativa)
+# print("Alunos acima da média:",aMediaPositiva)
+# print("Alunos abaixo da média:",aMediaNegativa)
+
+
+
+
+
+
+
